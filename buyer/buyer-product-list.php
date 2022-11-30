2f2701f29f8e6645691dd44ef6a4fcd1885df6ae
Gray <blackcat5523@gmail.com>
2022-11-30 15:19:41 +0800

    增加ＵＩ

--- buyer/buyer-product-list.php
+++ buyer/buyer-product-list.php
@@ -28,39 +28,39 @@ $page_start = ($page - 1) * $per_page;
 if (isset($_GET["category"])) {
   $pageCategory = $_GET["category"];
 
-    // 多項篩選
-    if(isset($_GET["min"])){
-      $min = $_GET["min"];
-      $max = $_GET["max"];
-      if (empty($min)) $min = 0;
-      if (empty($max)) $max = 99999;
-      $sql2 = "SELECT * FROM `product` WHERE `category` =  " . $_GET["category"] . " AND `product`.`price`>=$min AND `product`.`price`<=$max ORDER BY `product`.`price` DESC LIMIT $page_start, $per_page";
-      $sqlAll = "SELECT * FROM `product` WHERE `category` =  " . $_GET["category"] . " ORDER BY `product`.`create_time` DESC";
-    }else{
-      $sql2 = "SELECT * FROM `product` WHERE `category` =  " . $_GET["category"] . " ORDER BY `product`.`create_time` DESC LIMIT $page_start, $per_page";
-      $sqlAll = "SELECT * FROM `product` WHERE `category` =  " . $_GET["category"] . " ORDER BY `product`.`create_time` DESC";
-    }
-  
+  // 多項篩選
+  if (isset($_GET["min"])) {
+    $min = $_GET["min"];
+    $max = $_GET["max"];
+    if (empty($min)) $min = 0;
+    if (empty($max)) $max = 99999;
+    $sql2 = "SELECT * FROM `product` WHERE `category` =  " . $_GET["category"] . " AND `product`.`price`>=$min AND `product`.`price`<=$max ORDER BY `product`.`price` DESC LIMIT $page_start, $per_page";
+    $sqlAll = "SELECT * FROM `product` WHERE `category` =  " . $_GET["category"] . " ORDER BY `product`.`create_time` DESC";
+  } else {
+    $sql2 = "SELECT * FROM `product` WHERE `category` =  " . $_GET["category"] . " ORDER BY `product`.`create_time` DESC LIMIT $page_start, $per_page";
+    $sqlAll = "SELECT * FROM `product` WHERE `category` =  " . $_GET["category"] . " ORDER BY `product`.`create_time` DESC";
+  }
+
   $result = $conn->query($sql2);
   $resultAll = $conn->query($sqlAll);
   $userCount = $resultAll->num_rows;
-  
-// 價錢分類
-}elseif (isset($_GET["min"])) {
+
+  // 價錢分類
+} elseif (isset($_GET["min"])) {
   $min = $_GET["min"];
   $max = $_GET["max"];
-  
+
   if (empty($min)) $min = 0;
   if (empty($max)) $max = 99999;
-  
+
   $sql2 = "SELECT product.*, category.name AS category_name FROM product JOIN category ON product.category = category.id WHERE product.price >= $min AND product.price <=$max ORDER BY product.price";
   $sqlAll = "SELECT product.*, category.name AS category_name FROM product JOIN category ON product.category = category.id WHERE product.price >= $min AND product.price <=$max";
-      
-      
+
+
   $result = $conn->query($sql2);
   $resultAll = $conn->query($sqlAll);
   $userCount = $resultAll->num_rows;
-//全部
+  //全部
 } else {
   $sql2 = "SELECT * FROM `product` ORDER BY `product`.`create_time` DESC
   LIMIT $page_start, $per_page";
@@ -98,7 +98,7 @@ $totalPage = ceil($userCount / $per_page);
       height: 300vh;
     }
 
-    a{
+    a {
       text-decoration: none;
       color: #444;
     }
@@ -171,181 +171,190 @@ $totalPage = ceil($userCount / $per_page);
 
 <body>
   <nav class="main-nav d-flex bg-dark fixed-top shadow">
-      <a class="text-nowrap px-3 text-white text-decoration-none d-flex align-items-center justify-content-center logo flex-shrink-0 fs-4 text" href="">藝拍</a>
-      <div class="nav">
-            <a class="nav-link" href="#">首頁</a>
-            <a class="nav-link" href="../buyer/buyer-product-list.php">藝術品參觀</a>
-            <a class="nav-link" href="#">展覽空間</a>
-        </div>
-      <div class="position-absolute top-0 end-0">
-        <a class="btn btn-dark text-nowrap" href="logout.php">Sign out</a>
-      </div>
-    </nav>
-    <aside class="left-aside position-fixed bg-dark border-end">
-      <nav class="aside-menu">
-        <!-- <div class="pt-2 px-3 pb-2 d-flex justify-content-center text-white">
+    <a class="text-nowrap px-3 text-white text-decoration-none d-flex align-items-center justify-content-center logo flex-shrink-0 fs-4 text" href="">藝拍</a>
+    <div class="nav">
+      <a class="nav-link" href="#">首頁</a>
+      <a class="nav-link" href="../buyer/buyer-product-list.php">藝術品參觀</a>
+      <a class="nav-link" href="#">展覽空間</a>
+    </div>
+    <div class="position-absolute top-0 end-0">
+      <a class="btn btn-dark text-nowrap" href="logout.php">Sign out</a>
+    </div>
+  </nav>
+  <aside class="left-aside position-fixed bg-dark border-end">
+    <nav class="aside-menu">
+      <!-- <div class="pt-2 px-3 pb-2 d-flex justify-content-center text-white">
         Welcome <?= $_SESSION["user"]["account"] ?> !
       </div> -->
       <ul class="list-unstyled">
-      <a href="#" class=" align-items-center link-dark text-decoration-none ">
+        <a href="#" class=" align-items-center link-dark text-decoration-none ">
           <img src="./images/a.jpg" alt="" width="110" height="110" class="rounded-circle mx-auto">
         </a>
-          <h1 class="py-2 d-flex justify-content-center text-white">會員</h1>
-          <hr class="text-white">
-            <li class="active"><a href="../buyer/buyer.php?id=<?=$_SESSION["user"]["id"]?>" class="px-3 py-2"> <i class="fa-solid fa-face-smile fa-fw"></i>會員個人資料</a></li>               
-            <li><a href="../buyer/buyer-order-detail.php?id=<?=$_SESSION["user"]["id"]?>" class="px-3 py-2"><i class="fa-regular fa-file-lines fa-fw"></i>個人訂單檢視</a></li>
-            <li><a href="" class="px-3 py-2"><i class="fa-solid fa-barcode"></i>折扣卷</a></li>
-            <li><a href="" class="px-3 py-2"><i class="fa-solid fa-heart"></i>我的收藏</a></li>
+        <h1 class="py-2 d-flex justify-content-center text-white">會員</h1>
+        <hr class="text-white">
+        <li class="active"><a href="../buyer/buyer.php?id=<?= $_SESSION["user"]["id"] ?>" class="px-3 py-2"> <i class="fa-solid fa-face-smile fa-fw"></i>會員個人資料</a></li>
+        <li><a href="../buyer/buyer-order-detail.php?id=<?= $_SESSION["user"]["id"] ?>" class="px-3 py-2"><i class="fa-regular fa-file-lines fa-fw"></i>個人訂單檢視</a></li>
+        <li><a href="" class="px-3 py-2"><i class="fa-solid fa-barcode"></i>折扣卷</a></li>
+        <li><a href="" class="px-3 py-2"><i class="fa-solid fa-heart"></i>我的收藏</a></li>
       </ul>
 
-      </nav>
-    </aside>
-    <main class="main-content">
-      <div class="d-flex justify-content-between">
-        <h1>主選單</h1>
-      </div>
-      <div class="">
-        <div class="container">
+    </nav>
+  </aside>
+  <main class="main-content">
+    <div class="d-flex justify-content-between">
+      <h1>主選單</h1>
+    </div>
+    <div class="">
+      <div class="container">
 
-          <ul class="nav nav-tabs">
+        <ul class="nav nav-tabs">
+          <li class="nav-item">
+            <a class="nav-link <?php if (!isset($_GET["category"])) echo "active"; ?>" aria-current="page" href="buyer-product-list.php">全部</a>
+          </li>
+          <!--  -->
+          <?php foreach ($rowsCategory as $category) : ?>
             <li class="nav-item">
-              <a class="nav-link <?php if (!isset($_GET["category"])) echo "active"; ?>" aria-current="page" href="buyer-product-list.php">全部</a>
+              <a class="nav-link" <?php if (isset($_GET["category"]) && $_GET["category"] == $category["id"]) echo "active"; ?> href="buyer-product-list.php?category=<?= $category["id"] ?>"> <?= $category["name"] ?> </a>
             </li>
-            <!--  -->
-            <?php foreach ($rowsCategory as $category) : ?>
-              <li class="nav-item">
-                <a class="nav-link" <?php if (isset($_GET["category"]) && $_GET["category"] == $category["id"]) echo "active"; ?> href="buyer-product-list.php?category=<?= $category["id"] ?>"> <?= $category["name"] ?> </a>
-              </li>
-            <?php endforeach; ?>
-            <!--  -->
-          </ul>
-          <div class="py-2">
-            <!-- 表單 -->
-            <form action="buyer-product-list.php" method="GET">
-              <div class="row align-items-center g-2">
-                <?php if (isset($_GET["min"])) : ?>
-                  <div class="col-auto">
-                    <a class="btn btn-dark" href="./buyer-product-list.php">Back</a>
-                  </div>
-                <?php endif; ?>
+          <?php endforeach; ?>
+          <!--  -->
+        </ul>
+        <div class="py-2">
+          <!-- 表單 -->
+          <form action="buyer-product-list.php" method="GET">
+            <div class="row align-items-center g-2">
+              <?php if (isset($_GET["min"])) : ?>
                 <div class="col-auto">
-                  <input type="number" class="form-control text-center" name="min" placeholder="輸入最小金額" value="<?php
-                  if (isset($_GET["min"])) echo $price; ?>">
+                  <a class="btn btn-dark" href="./buyer-product-list.php">Back</a>
                 </div>
-                <div class="col-auto">
-                  ~
+              <?php endif; ?>
+              <div class="col-auto">
+                <input type="number" class="form-control text-center" name="min" placeholder="輸入最小金額" value="<?php
+                                                                                                              if (isset($_GET["min"])) echo $price; ?>">
+              </div>
+              <div class="col-auto">
+                ~
+              </div>
+              <div class="col-auto">
+                <input type="number" class="form-control text-center" name="max" placeholder="輸入最大金額" value="<?php
+                                                                                                              if (isset($_GET["max"])) echo $price; ?>">
+              </div>
+              <div class="col-auto">
+                <div class="form-check form-check-into">
+                  <input class="form-check-input" type="radio" name="category-radio" value="1">
+                  <label class="form-check-label" for="inlineRadio1">水墨</label>
                 </div>
-                <div class="col-auto">
-                  <input type="number" class="form-control text-center" name="max" placeholder="輸入最大金額" value="<?php
-                  if (isset($_GET["max"])) echo $price; ?>">
-                </div>
-                
-                <div class="col-auto">
-                  <button class="btn btn-dark" type="submit">篩選</button>
+                <div class="form-check form-check-into">
+                  <input class="form-check-input" type="radio" name="category-radio" value="2">
+                  <label class="form-check-label" for="inlineRadio1">拼貼</label>
                 </div>
               </div>
-            </form>
-            <?php ?>
-          </div>
-          <div class="py-2 text-end">
-            共<?= $productCount ?>項
-          </div>
-          <?php if ($userCount > 0) : ?>
-            <div class="row d-flex flex-wrap">
-              <?php foreach ($rows as $row) :
-              ?>
+              <div class="row-auto">
+                <button class="btn btn-dark" type="submit">篩選</button>
+              </div>
+            </div>
+          </form>
+          <?php ?>
+        </div>
+        <div class="py-2 text-end">
+          共<?= $productCount ?>項
+        </div>
+        <?php if ($userCount > 0) : ?>
+          <div class="row d-flex flex-wrap">
+            <?php foreach ($rows as $row) :
+            ?>
 
-                <div class="col-lg-3 col-md-6 py-3">
-                  <div class="card position-relative">
-                    <a class="like position-absolute"></a>
-                    <figure class="ratio ratio-16x9">
-                      <img class="object-cover" src="../product/images/<?= $row["images"] ?>" alt="">
-                    </figure>
-                    <div class="px-2 pb-3">
-                      <div class="pb-2 text-primary">
-                        <a href="buyer-product-list.php?category=<?= $row["category"] ?>">
-                          <?php
-                          if ($row["category"] == "1") {
-                            echo "ink";
-                          } elseif ($row["category"] == "2") {
-                            echo "collage";
-                          } elseif ($row["category"] == "3") {
-                            echo "canvas";
-                          } elseif ($row["category"] == "4") {
-                            echo "watercolor";
-                          } elseif ($row["category"] == "5") {
-                            echo "Sculpture";
-                          } elseif ($row["category"] == "6") {
-                            echo "digit";
-                          }
-                          ?>
+              <div class="col-lg-3 col-md-6 py-3">
+                <div class="card position-relative">
+                  <a class="like position-absolute"></a>
+                  <figure class="ratio ratio-16x9">
+                    <img class="object-cover" src="../product/images/<?= $row["images"] ?>" alt="">
+                  </figure>
+                  <div class="px-2 pb-3">
+                    <div class="pb-2 text-primary">
+                      <a href="buyer-product-list.php?category=<?= $row["category"] ?>">
+                        <?php
+                        if ($row["category"] == "1") {
+                          echo "ink";
+                        } elseif ($row["category"] == "2") {
+                          echo "collage";
+                        } elseif ($row["category"] == "3") {
+                          echo "canvas";
+                        } elseif ($row["category"] == "4") {
+                          echo "watercolor";
+                        } elseif ($row["category"] == "5") {
+                          echo "Sculpture";
+                        } elseif ($row["category"] == "6") {
+                          echo "digit";
+                        }
+                        ?>
 
-                        </a>
-                      </div>
-                      <h3 class="text-center h4">
-                        <?= $row["name"] ?>
-                      </h3>
-                      <div class="text-end">
-                        <?= $row["price"] ?>
-                      </div>
+                      </a>
+                    </div>
+                    <h3 class="text-center h4">
+                      <?= $row["name"] ?>
+                    </h3>
+                    <div class="text-end">
+                      <?= $row["price"] ?>
                     </div>
                   </div>
                 </div>
+              </div>
 
-              <?php endforeach; ?>
-            </div>
-          <?php endif; ?>
-
-        </div>
-        <!-- 頁碼選單 -->
-        <?php if (isset($_GET["category"])) : ?>
-          <nav aria-label="Page navigation example">
-            <ul class="pagination">
-              <?php for ($i = 1; $i <= $totalPage; $i++) : ?>
-                <li class="page-item 
-            <?php
-                if ($i == $page) echo "active";
-            ?>">
-                  <a class="page-link" href="buyer-product-list.php?category=<?= $_GET["category"] ?>&page=<?= $i ?>"><?= $i ?></a>
-                </li>
-              <?php endfor; ?>
-            </ul>
-          </nav>
-          <?php elseif ((isset($_GET["min"]))&&(isset($_GET["max"]))) : ?>
-          <nav aria-label="Page navigation example">
-            <ul class="pagination">
-              <?php for ($i = 1; $i <= $totalPage; $i++) : ?>
-                <li class="page-item 
-            <?php
-                if ($i == $page) echo "active";
-            ?>">
-                  <a class="page-link" href="buyer-product-list.php?min=<?= $_GET["min"] ?>&max=<?= $_GET["max"] ?>&page=<?= $i ?>"><?= $i ?></a>
-                </li>
-              <?php endfor; ?>
-            </ul>
-          </nav>
-        <?php else : ?>
-          <nav aria-label="Page navigation example">
-            <ul class="pagination">
-              <?php for ($i = 1; $i <= $totalPage; $i++) : ?>
-                <li class="page-item 
-            <?php
-                if ($i == $page) echo "active";
-            ?>">
-                  <a class="page-link" href="buyer-product-list.php?page=<?= $i ?>"><?= $i ?></a>
-                </li>
-              <?php endfor; ?>
-            </ul>
-          </nav>
+            <?php endforeach; ?>
+          </div>
         <?php endif; ?>
 
       </div>
-      </div>
-      </div>
+      <!-- 頁碼選單 -->
+      <?php if (isset($_GET["category"])) : ?>
+        <nav aria-label="Page navigation example">
+          <ul class="pagination">
+            <?php for ($i = 1; $i <= $totalPage; $i++) : ?>
+              <li class="page-item 
+            <?php
+              if ($i == $page) echo "active";
+            ?>">
+                <a class="page-link" href="buyer-product-list.php?category=<?= $_GET["category"] ?>&page=<?= $i ?>"><?= $i ?></a>
+              </li>
+            <?php endfor; ?>
+          </ul>
+        </nav>
+      <?php elseif ((isset($_GET["min"])) && (isset($_GET["max"]))) : ?>
+        <nav aria-label="Page navigation example">
+          <ul class="pagination">
+            <?php for ($i = 1; $i <= $totalPage; $i++) : ?>
+              <li class="page-item 
+            <?php
+              if ($i == $page) echo "active";
+            ?>">
+                <a class="page-link" href="buyer-product-list.php?min=<?= $_GET["min"] ?>&max=<?= $_GET["max"] ?>&page=<?= $i ?>"><?= $i ?></a>
+              </li>
+            <?php endfor; ?>
+          </ul>
+        </nav>
+      <?php else : ?>
+        <nav aria-label="Page navigation example">
+          <ul class="pagination">
+            <?php for ($i = 1; $i <= $totalPage; $i++) : ?>
+              <li class="page-item 
+            <?php
+              if ($i == $page) echo "active";
+            ?>">
+                <a class="page-link" href="buyer-product-list.php?page=<?= $i ?>"><?= $i ?></a>
+              </li>
+            <?php endfor; ?>
+          </ul>
+        </nav>
+      <?php endif; ?>
 
-    </main>
+    </div>
+    </div>
+    </div>
 
-    <!-- <footer class="footer">
+  </main>
+
+  <!-- <footer class="footer">
       <div class="container-fruid d-flex justify-content-center">
         <div class="menu list-unstyled inline-flex">
 
